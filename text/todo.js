3ef3e590817d29b6a79d861fe164a16f250c876b
Prabin Amatya <pnamatya@gmail.com>
2015-05-24 16:43:15 -0500

    databinding

---
+++ text/todo.js
@@ -0,0 +1,26 @@
+/**
+ * Created by pnamatya on 5/24/15.
+ */
+
+function TodoCtrl($scope) {
+    $scope.todos = [
+        {text:'Learn AngularJS', done:false},
+        {text:'Build an app', done:false}
+    ];
+
+    $scope.getTotalTodos = function () {
+        return $scope.todos.length;
+    };
+
+    $scope.clearCompleted = function () {
+        $scope.todos = _.filter($scope.todos, function(todo){
+            return !todo.done;
+        });
+    };
+
+    $scope.addTodo = function () {
+        $scope.todos.push({text:$scope.formTodoText, done:false});
+        $scope.formTodoText = '';
+    };
+}
+
